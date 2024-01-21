642bd024f24e2dc1a6fbeab5a176c82fad125a55
Jarkko Parviainen <jarkko.parviainen80@gmail.com>
2024-01-21 17:36:08 +0100

    Fixed the position of the defaults.

--- src/node_query_system/search_criteria/custom.cpp
+++ src/node_query_system/search_criteria/custom.cpp
@@ -13,9 +13,9 @@ UtilityAICustomSearchCriterion::~UtilityAICustomSearchCriterion() {
 }
 
 void UtilityAICustomSearchCriterion::apply_criterion( Node* node, bool& filter_out, float& score ) {
-    if( !has_method("apply_criterion")) return;
     _score = 1.0f;
     _is_filtered = false;
+    if( !has_method("apply_criterion")) return;
     call("apply_criterion", node);
     filter_out = _is_filtered;
     score = _score;
