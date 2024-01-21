7472f0c6f585a5679f76eb5729c5cdd5e5f9d180
JarkkoPar <64613837+JarkkoPar@users.noreply.github.com>
2024-01-21 16:31:38 +0100

    Set the defaults for the custom search criterion.

--- src/node_query_system/search_criteria/custom.cpp
+++ src/node_query_system/search_criteria/custom.cpp
@@ -14,6 +14,8 @@ UtilityAICustomSearchCriterion::~UtilityAICustomSearchCriterion() {
 
 void UtilityAICustomSearchCriterion::apply_criterion( Node* node, bool& filter_out, float& score ) {
     if( !has_method("apply_criterion")) return;
+    _score = 1.0f;
+    _is_filtered = false;
     call("apply_criterion", node);
     filter_out = _is_filtered;
     score = _score;
